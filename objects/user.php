6df71b5d5f040ee27c4f9b403eb2179f41ef4ddd
ToniGomez <agomezj@cifpfbmoll.eu>
2021-12-01 12:39:32 +0100

    Validation of the email address

--- objects/user.php
+++ objects/user.php
@@ -78,18 +78,18 @@ class User{
         $this->created=date('Y-m-d H:i:s');
 
         // insert query
-        $query = "INSERT INTO
-                " . $this->table_name . "
+        $query = "INSERT INTO " . $this->table_name . "
             SET
-                firstname = :firstname,
-                lastname = :lastname,
-                email = :email,
-                contact_number = :contact_number,
-                address = :address,
-                password = :password,
-                access_level = :access_level,
-                status = :status,
-                created = :created";
+        firstname = :firstname,
+        lastname = :lastname,
+        email = :email,
+        contact_number = :contact_number,
+        address = :address,
+        password = :password,
+        access_level = :access_level,
+                access_code = :access_code,
+        status = :status,
+        created = :created";
 
         // prepare the query
         $stmt = $this->conn->prepare($query);
@@ -102,6 +102,7 @@ class User{
         $this->address=htmlspecialchars(strip_tags($this->address));
         $this->password=htmlspecialchars(strip_tags($this->password));
         $this->access_level=htmlspecialchars(strip_tags($this->access_level));
+        $this->access_code=htmlspecialchars(strip_tags($this->access_code));
         $this->status=htmlspecialchars(strip_tags($this->status));
 
         // bind the values
@@ -116,6 +117,7 @@ class User{
         $stmt->bindParam(':password', $password_hash);
 
         $stmt->bindParam(':access_level', $this->access_level);
+        $stmt->bindParam(':access_code', $this->access_code);
         $stmt->bindParam(':status', $this->status);
         $stmt->bindParam(':created', $this->created);
 
