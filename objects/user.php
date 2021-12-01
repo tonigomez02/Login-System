b6947e605e2bd5e6631eff7202a9e210be38f1e0
ToniGomez <agomezj@cifpfbmoll.eu>
2021-12-01 12:35:28 +0100

    Showing deatils of the registred users on the admin section

--- objects/user.php
+++ objects/user.php
@@ -134,4 +134,53 @@ class User{
         print_r($stmt->errorInfo());
         echo "</pre>";
     }
+
+    // read all user records
+    function readAll($from_record_num, $records_per_page){
+
+        // query to read all user records, with limit clause for pagination
+        $query = "SELECT
+                id,
+                firstname,
+                lastname,
+                email,
+                contact_number,
+                access_level,
+                created
+            FROM " . $this->table_name . "
+            ORDER BY id DESC
+            LIMIT ?, ?";
+
+        // prepare query statement
+        $stmt = $this->conn->prepare( $query );
+
+        // bind limit clause variables
+        $stmt->bindParam(1, $from_record_num, PDO::PARAM_INT);
+        $stmt->bindParam(2, $records_per_page, PDO::PARAM_INT);
+
+        // execute query
+        $stmt->execute();
+
+        // return values
+        return $stmt;
+    }
+
+    // used for paging users
+    public function countAll(){
+
+        // query to select all user records
+        $query = "SELECT id FROM " . $this->table_name . "";
+
+        // prepare query statement
+        $stmt = $this->conn->prepare($query);
+
+        // execute query
+        $stmt->execute();
+
+        // get number of rows
+        $num = $stmt->rowCount();
+
+        // return row count
+        return $num;
+    }
 }
